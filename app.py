1993a972e1ed332869528e43d67be67b3b1f7093
Nithin Kumar K <92811211+nithin-developer@users.noreply.github.com>
2025-11-24 10:52:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+print("Helloo")
+print("Hiiiiii")
+print("VVCE")
+print("abcd")
+print("Helloo")
+print("Hiiiiii")
+print("VVCE")
+print("abcd")
